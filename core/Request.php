503f2bd98e886a7417370988df3fc2003a30ab6d
rasmus <r_emil@live.dk>
2022-10-20 22:27:41 +0200

    remove curly fries

--- core/Request.php
+++ core/Request.php
@@ -30,19 +30,16 @@ class Request {
     }
 
     public function getBody() {
+        
         $body = [];
 
-        if ($this->method() === 'get') {
-            foreach ($_GET as $key => $value) {
+        if ($this->method() === 'get')
+            foreach ($_GET as $key => $value)
                 $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-        }
 
-        if ($this->method() === 'post') {
-            foreach ($_POST as $key => $value) {
+        if ($this->method() === 'post') 
+            foreach ($_POST as $key => $value)
                 $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
-            }
-        }
 
         return $body;
     }

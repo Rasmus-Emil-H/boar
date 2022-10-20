e3e523cc11e0507de10d57ab5b9661947f526d9c
rasmus <r_emil@live.dk>
2022-10-20 21:47:37 +0200

    rm dotenv

--- migrations.php
+++ migrations.php
@@ -4,9 +4,6 @@ use app\core\Application;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 $config = [
     'authenticationClass' => \app\models\User::class,
     'pdo' => [

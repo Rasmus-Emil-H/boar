10ae33b10d7520de163ea5c60b176962602b6be6
troldefar <r_emil@live.dk>
2022-10-29 20:28:38 +0200

    cook

--- core/Cookie.php
+++ core/Cookie.php
@@ -14,10 +14,8 @@ class Cookie {
         $_COOKIE[$key] = password_hash($value, PASSWORD_DEFAULT);
     }
 
-    public function getCookie(string $key): string {
-        $cookie = $_COOKIE[$key] ?? '';
-        if ( !password_check($cookie, PASSWORD_DEFAULT) ) exit('Invalid cookie');
-        return $_COOKIE[$key];
+    public function getCookie(string $cookie): string {
+        return $_COOKIE[$cookie] ?? '';
     }
 
 }

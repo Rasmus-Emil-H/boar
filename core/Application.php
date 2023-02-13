04ce49ee9b6c2cec7138d3dda3942bf1e1020134
troldefar <r_emil@live.dk>
2023-02-13 10:54:21 +0100

    class checkerr

--- core/Application.php
+++ core/Application.php
@@ -3,7 +3,6 @@
 /*******************************
  * Bootstrap application 
  * AUTHOR: RE_WEB
- * Entrypoint for application
  * @package app\core\application
 *******************************/
 
@@ -17,18 +16,25 @@ class Application {
     */
 
     public static string $ROOT_DIR;
+
     public string $layout = 'main';
     public string $authenticationClass;
-
+    
+    public string $trackingAPI = 'https://tracking.autologik.dk/fetch';
+    public string $trackingAPIKey = 'MC45NzE3NjM1OTgwMDg4MzUy';
+    
+    public string $bookAPI = 'https://dev.book.autologik.dk/api';
+    public string $bookAPIKey = 'MC45NzE3NjM1OTgwMDg4MzUy';
+    
     public Router $router;
     public Request $request;
     public Response $response;
+    public ?Controller $controller = null;
     public Session $session;
     public Database $database;
-    public View $view;
-    public Authentication $authentication;
-    public ?Controller $controller = null;
     public ?DbModel $user;
+    public View $view;
+    public Regex $regex;
 
     /**
      * Application states  
@@ -49,17 +55,33 @@ class Application {
     public const UPLOAD_FOLDER = __DIR__.'/uploads/';
 
     public function __construct(string $rootPath, array $pdoConfigurations) {
+        
         $this->authenticationClass = $pdoConfigurations['authenticationClass'];
+
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
-        $this->request          = new Request();
-        $this->response         = new Response();
-        $this->router           = new Router($this->request, $this->response);
-        $this->session          = new Session();
-        $this->database         = new Database($pdoConfigurations['pdo']);
-        $this->view             = new View();
-        $this->authentication   = new Authentication();
-        $this->authentication->checkUserBasedOnSession();
+        
+        $this->request   = new Request();
+        $this->response  = new Response();
+        $this->regex     = new Regex($this->request->getPath());
+        $this->router    = new Router($this->request, $this->response);
+        $this->session   = new Session();
+        $this->database  = new Database($pdoConfigurations['pdo']);
+        $this->view      = new View();
+
+        $this->checkUserBasedOnSession();
+
+    }
+
+    public function checkUserBasedOnSession(): void {
+        $primaryValue = $this->session->get('user');
+        !$primaryValue ? $this->user = null : $this->setApplicationUser($primaryValue);
+    }
+
+    public function setApplicationUser(string $primaryValue): void {
+        $authenticationClass = new $this->authenticationClass();
+        $primaryKey = $authenticationClass->getPrimaryKey();
+        $this->user = $authenticationClass->findOne([$primaryKey => $primaryValue], $authenticationClass->tableName());
     }
 
     /**
@@ -80,11 +102,15 @@ class Application {
         }
     }
 
+    public function classCheck(string $class): void {
+        if(!class_exists($class)) $this->response->setResponse(400, 'application/json', ['msg' => 'bad request']);
+    }
+
     /**
      * Exception code handler
     */
 
-    protected function exceptionCodeHandler(mixed $code) {
+    protected function exceptionCodeHandler($code) {
         if( !is_int($code) ) 
             throw new \Exception('Invalid status code. Must be int, however ' . gettype($code) . ' is provided.');
     }
@@ -100,6 +126,23 @@ class Application {
 
     public function setController(Controller $controller): void {
         $this->controller = $controller;
+    }
+
+    public function login(DbModel $user): bool {
+        $this->user = $user;
+        $primaryKey = $user->getPrimaryKey();
+        $primaryValue = $user->{$primaryKey};
+        $this->session->set('user', $primaryValue);
+        return true;
+    }
+
+    public function logout(): void {
+        $this->user = null;
+        $this->session->removeSessionProperty('user');
+    }
+
+    public static function isGuest(): bool {
+        return is_null(self::$app->user);
     }
     
 }

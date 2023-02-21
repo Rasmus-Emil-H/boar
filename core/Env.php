69c42c1a65cb21c71d58d97136ba4b3885360677
troldefar <r_emil@live.dk>
2023-02-21 15:35:13 +0100

    ph for add. configs needed

---
+++ core/Env.php
@@ -0,0 +1,18 @@
+<?php
+
+/*******************************
+ * Bootstrap Env
+ * Temp storage for additional keys 
+ * AUTHOR: RE_WEB
+ * @package app\core\Env
+*/
+
+namespace app\core;
+
+class Env {
+
+    public function get(string $key): string {
+        return $this->{$key};
+    }
+
+}

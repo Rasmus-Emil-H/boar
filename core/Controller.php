23cf9aa34e94dc651484fa180914748d8aa62a76
troldefar <r_emil@live.dk>
2023-03-09 13:21:30 +0100

    setter

--- core/Controller.php
+++ core/Controller.php
@@ -43,6 +43,28 @@ class Controller {
         return $this->middlewares;
     }
 
+    public function save() {
+        foreach ( Application::$app->request->getPHPInput() as $key => $value ) {
+            foreach ( $value as $objectKey => $objectValues ) {
+                if ( $objectKey === 'undefined' ) continue;
+                $exp = explode('-', $objectKey);
+                $model = $exp[0].'Model';
+                $prefix = self::MODEL_PREFIX.ucfirst($model);
+                Application::$app->classCheck($prefix);
+                $obj = new $prefix();
+                $static = $obj->findOne([$obj->getPrimaryKey() => $exp[1]], $obj->tableName());
+                if(!$static) {
+                    $static = new $prefix();
+                    $pk = $static->getPrimaryKey();
+                    $static->{$pk} = $exp[1];
+                }
+                $static->setAttributes((array)$objectValues);
+			    $static->save();
+            }
+        }
+		Application::$app->response->setResponse(200, 'application/json', ['msg' => 'saved']);
+	}
+
     public function remove() {
 		$id = Application::$app->request->getPHPInput();
 		$reqModel = explode('-', $id->id);
@@ -53,21 +75,6 @@ class Controller {
 		$static   = $obj->findOne([$obj->getPrimaryKey() => $reqModel[1]], $obj->tableName());
 		$static->remove();
         Application::$app->response->setResponse(204, 'application/json', ['msg' => 'deleted']);
-	}
-
-    public function save() {
-		foreach(Application::$app->request->getPHPInput() as $key => $value) {
-			$exp = explode('-', $key);
-			$model = $exp[0].'Model';
-            $prefix = self::MODEL_PREFIX.ucfirst($model);
-            Application::$app->classCheck($prefix);
-            $obj = new $prefix();
-			$static = $obj->findOne([$obj->getPrimaryKey() => $exp[1]], $obj->tableName());
-            if(!$static) $static = new $prefix();
-			$static->setAttributes([$exp[0], $value, $exp[2] ?? '']);
-			$static->save();
-		}
-		Application::$app->response->setResponse(200, 'application/json', ['msg' => 'saved']);
 	}
 
 }

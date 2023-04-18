53fc58ae8cfe60588eaa18fabc914d6df522c9ae
rasmus <r_emil@live.dk>
2023-04-18 08:51:08 +0200

    static render class

---
+++ core/html/Render.php
@@ -0,0 +1,20 @@
+<?php
+
+/*******************************
+ * Bootstrap application 
+ * AUTHOR: RE_WEB
+ * A static object for various DOM nodes i have no current plans for
+ * @package app\core\render
+*******************************/
+
+namespace app\core\render;
+
+use app\core\Application;
+
+class Render {
+
+    public static function menu() {
+        var_dump(Application::$app);
+    }
+
+}

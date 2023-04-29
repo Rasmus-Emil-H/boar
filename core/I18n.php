6c4706e290904f8768b3976de320f3803f1abc1f
troldefar <r_emil@live.dk>
2023-04-29 15:20:50 +0200

    useuse

--- core/I18n.php
+++ core/I18n.php
@@ -8,11 +8,13 @@
 
 namespace app\core;
 
+use \app\models\LanguageModel;
+
 class I18n {
 
     public function __construct() {
         $this->currentLanguage = Application::$app->session->get('language');
-        $this->languages = \app\models\LanguageModel::all();
+        $this->languages = LanguageModel::all();
         echo '<pre>';var_dump($this->languages);echo '</pre>';
         // $this->languageID = Application::$app->connection->select("t_languages l", ["l.languageID"])->where(["l.language" => $this->currentLanguage])->execute($this)[0]['languageID']??0;
         // $this->languageID = $this->languageID;

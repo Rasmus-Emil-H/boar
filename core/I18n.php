23f94d0ae29d66f43711d23c68c936956951e4f4
troldefar <r_emil@live.dk>
2023-04-29 13:01:20 +0200

    inject language instead

--- core/I18n.php
+++ core/I18n.php
@@ -8,27 +8,27 @@
 
 namespace app\core;
 
-class I18n extends \app\models\LanguageModel {
+class I18n {
 
-    protected string $currentLanguage;
-    protected string $languageID;
+    private \app\models\LanguageModel $languages;
 
     public function __construct() {
-        var_dump($this->get());exit;
+        $this->languages = new \app\models\LanguageModel();
         $this->currentLanguage = Application::$app->session->get('language');
-        $res = Application::$app->database->select("t_languages l", ["l.languageID"])->where(["l.language" => $this->currentLanguage])->execute();
-        $this->languageID = Application::$app->database->select("t_languages l", ["l.languageID"])->where(["l.language" => $this->currentLanguage])->execute()[0]['languageID']??0;
-        $this->languageID = $this->languageID;
+        $res = $this->languages->get();
+        echo '<pre>';var_dump($res);echo '</pre>';exit;
+        // $this->languageID = Application::$app->connection->select("t_languages l", ["l.languageID"])->where(["l.language" => $this->currentLanguage])->execute($this)[0]['languageID']??0;
+        // $this->languageID = $this->languageID;
     }
 
     public function translate(string $toTranslate): string {
-        return Application::$app->database->select("t_translations t", ["t.translation"])->where(["t.languageID" => $this->languageID, "t.translationKey" => $toTranslate])->execute()[0]['translation'] ?? $this->registerMissingTranslation($toTranslate);
+        return Application::$app->connection->select("t_translations t", ["t.translation"])->where(["t.languageID" => $this->languageID, "t.translationKey" => $toTranslate])->execute()[0]['translation'] ?? $this->registerMissingTranslation($toTranslate);
     }
 
     public function registerMissingTranslation(string $missingTranslation): string {
-        foreach ( $this->get() as $language ) {
-            var_dump($language);
-        }
+        // foreach ( $this->get() as $language ) {
+        //     var_dump($language);
+        // }
         return 'Missing translation';
     }
 }

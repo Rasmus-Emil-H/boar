db26c71b16279ccbf836765e8c51c8975859df4a
troldefar <r_emil@live.dk>
2023-05-01 00:49:27 +0200

    cc

--- core/Env.php
+++ core/Env.php
@@ -11,12 +11,6 @@ namespace app\core;
 
 class Env {
 
-    public string $trackingAPI = 'https://tracking.autologik.dk/fetch';
-    public string $trackingAPIKey = 'MC45NzE3NjM1OTgwMDg4MzUy';
-    
-    public string $bookAPI = 'https://dev.book.autologik.dk/api';
-    public string $bookAPIKey = 'MC45NzE3NjM1OTgwMDg4MzUy';
-
     public function get(string $key): string {
         return $this->{$key};
     }

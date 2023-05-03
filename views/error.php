7a9ef057630b4311d58dd5706e16d1e88e534f4f
troldefar <r_emil@live.dk>
2023-05-03 08:37:39 +0200

    err

--- views/error.php
+++ views/error.php
@@ -1,5 +1,5 @@
-<div class="body-height align-center">
-    <h6 class="text-center pt-4 alert alert-danger body-height w-100 align-center text-center">
-        <?= $exception->getMessage(); ?>
-    </h6>
+<div class="body-height">
+    <h2 class="text-center pt-4 alert alert-danger body-height w-100 center">
+        ⚒️ <?= $exception->getMessage(); ?> ⚒️
+    </h2>
 </div>

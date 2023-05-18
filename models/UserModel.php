20f705a43489f0e6a35d24c8ce6b91cae54ab433
troldefar <r_emil@live.dk>
2023-05-18 13:18:33 +0200

    rm mv

--- models/UserModel.php
+++ models/UserModel.php
@@ -8,10 +8,40 @@
 
 namespace app\models;
 
-use app\core\DbModel;
+use \app\core\database\Entity;
+use \app\core\Application;
 
-abstract class UserModel extends DbModel {
+class UserModel extends Entity {
 
-    abstract public function getDisplayName(): string;
+    const keyID     = 'user_id';
+	const tableName = 'users';
+	
+	/*
+	 * Tablename
+	 * @return string
+	*/
+	
+	public function getTableName(): string {
+		return 'users';
+	}
+	
+	/*
+	 * Primary key
+	 * @return string
+	*/
+	
+	public function getKeyField(): string {
+		return 'user_id';
+	}
+
+	public function getForeignKeys(): array {
+		return [];
+	}
+
+	public static function search(array $criterias, array $values = ['*'], array $additionalQueryBuilding = []): array {
+        $rows = Application::$app->connection->select('sessions', $values)->whereClause($criterias);
+        $rows = $rows->execute();
+        return self::load(array_column($rows, static::keyID));
+    }
 
 }

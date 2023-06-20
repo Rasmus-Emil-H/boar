db00cd7c5a55c0fe7ed60498c219aa68755fe712
rasmus <r_emil@live.dk>
2023-06-20 18:43:50 +0200

    migration

--- migrations/add_meta_table_2019_01_03_0001.php
+++ migrations/add_meta_table_2019_01_03_0001.php
@@ -10,14 +10,14 @@
 
 use \app\core\Application;
 
-class add_users_table_2018_12_14_0001 {
+class add_meta_table_2019_01_03_0001 {
 
     public function up() {
-        $SQL = "CREATE TABLE IF NOT EXISTS Users (
-            UserID INT AUTO_INCREMENT PRIMARY KEY,
-            Email VARCHAR(255) NOT NULL,
-            Name VARCHAR(255) NOT NULL,
-            Status INT(1) DEFAULT 0,
+        $SQL = "CREATE TABLE IF NOT EXISTS Meta (
+            MetaId INT AUTO_INCREMENT PRIMARY KEY,
+            EntityType VARCHAR(255) NOT NULL,
+            EntityID VARCHAR(255) NOT NULL,
+            Data INT(1) DEFAULT 0,
             CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
         Application::$app->connection->rawSQL($SQL);
         Application::$app->connection->execute();

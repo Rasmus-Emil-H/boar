972089e4bfcbc1fd3a97edb3f84228846376b6dd
rasmus <r_emil@live.dk>
2023-06-20 18:45:01 +0200

    pw

--- migrations/add_password_to_user_table_2018_12_14_0001.php
+++ migrations/add_password_to_user_table_2018_12_14_0001.php
@@ -13,13 +13,13 @@ use \app\core\Application;
 class add_password_to_user_table_2018_12_14_0001 {
 
     public function up() {
-        $SQL = "ALTER TABLE Users ADD COLUMN password VARCHAR(255) ";
+        $SQL = "ALTER TABLE Users ADD COLUMN Password VARCHAR(255) ";
         Application::$app->connection->rawSQL($SQL);
         Application::$app->connection->execute();
     }
 
     public function down() {
-        $SQL = "ALTER TABLE Users DROP COLUMN password;";
+        $SQL = "ALTER TABLE Users DROP COLUMN Password;";
         Application::$app->connection->rawSQL($SQL);
         Application::$app->connection->execute();
     }

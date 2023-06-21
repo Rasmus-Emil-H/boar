d19c4af613c33e97d8f9260ba3f1a68e27c50a90
rasmus <r_emil@live.dk>
2023-06-21 17:56:28 +0200

    dev ips

--- config/Config.php
+++ config/Config.php
@@ -14,7 +14,7 @@ class Config {
 
     public function get(string $key): mixed {
         $config = file_get_contents(Application::$app::$ROOT_DIR.'/static/setup.json', 'R');
-        return json_decode($config)->$key;
+        return json_decode($config)->$key ?? throw new \Exception('Invalid config key');
     }
 
 }

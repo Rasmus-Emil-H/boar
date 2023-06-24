cc1de9a1d44d836f49be99d127ab71f08609477a
troldefar <r_emil@live.dk>
2023-06-24 10:02:08 +0200

    store css

--- views/login.php
+++ views/login.php
@@ -2,16 +2,27 @@
   /** @var $model app\models\User */
 ?>
 
-<div class="card p-4">
-  <form method="POST">
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <button type="submit" class="btn btn-primary w-100">Submit</button>
-  </form>
-</div>
+   <div class="screen">
+      <div class="screen__content">
+         <form class="login">
+            <div class="login__field">
+               <i class="login__icon fas fa-user"></i>
+               <input type="text" class="login__input" placeholder="Email">
+            </div>
+            <div class="login__field">
+               <i class="login__icon fas fa-lock"></i>
+               <input type="password" class="login__input" placeholder="Password">
+            </div>
+            <button class="button login__submit">
+            <span class="button__text">Login</span>
+            <i class="button__icon fas fa-chevron-right"></i>
+            </button>       
+         </form>
+      </div>
+      <div class="screen__background">
+         <span class="screen__background__shape screen__background__shape4"></span>
+         <span class="screen__background__shape screen__background__shape3"></span>    
+         <span class="screen__background__shape screen__background__shape2"></span>
+         <span class="screen__background__shape screen__background__shape1"></span>
+      </div>
+   </div>

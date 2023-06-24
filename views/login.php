da976a4c40208f4dea9850a1684dcb16f4e11cd5
troldefar <r_emil@live.dk>
2023-06-24 10:44:45 +0200

    haircur fade

--- views/login.php
+++ views/login.php
@@ -2,27 +2,27 @@
   /** @var $model app\models\User */
 ?>
 
-   <div class="screen">
-      <div class="screen__content">
-         <form class="login">
-            <div class="login__field">
-               <i class="login__icon fas fa-user"></i>
-               <input type="text" class="login__input" placeholder="Email">
-            </div>
-            <div class="login__field">
-               <i class="login__icon fas fa-lock"></i>
-               <input type="password" class="login__input" placeholder="Password">
-            </div>
-            <button class="button login__submit">
-            <span class="button__text">Login</span>
-            <i class="button__icon fas fa-chevron-right"></i>
-            </button>       
-         </form>
-      </div>
-      <div class="screen__background">
-         <span class="screen__background__shape screen__background__shape4"></span>
-         <span class="screen__background__shape screen__background__shape3"></span>    
-         <span class="screen__background__shape screen__background__shape2"></span>
-         <span class="screen__background__shape screen__background__shape1"></span>
-      </div>
-   </div>
+<div class="screen fade-in">
+  <div class="screen__content">
+      <form class="login" method="POST">
+        <div class="login__field">
+            <i class="login__icon fas fa-user"></i>
+            <input type="text" class="login__input" name="email" placeholder="Email">
+        </div>
+        <div class="login__field">
+            <i class="login__icon fas fa-lock"></i>
+            <input type="password" class="login__input" name="password" placeholder="Password">
+        </div>
+        <button class="button login__submit">
+        <span class="button__text">Login</span>
+        <i class="button__icon fas fa-chevron-right"></i>
+        </button>       
+      </form>
+  </div>
+  <div class="screen__background">
+      <span class="screen__background__shape screen__background__shape4"></span>
+      <span class="screen__background__shape screen__background__shape3"></span>    
+      <span class="screen__background__shape screen__background__shape2"></span>
+      <span class="screen__background__shape screen__background__shape1"></span>
+  </div>
+</div>

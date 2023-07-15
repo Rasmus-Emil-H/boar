50511672d883c0d0d240f3d13e1136edff378f2f
troldefar <r_emil@live.dk>
2023-07-15 08:58:47 +0200

    LIM

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,11 +1,11 @@
 <?php 
   require_once(dirname(__DIR__, 1).'/includes/header.php');
-  Use \app\core\Application;
 ?>
-<?php if (Application::$app->session->getFlashMessage('success')): ?>
+<?php if (\app\core\Application::$app->session->getFlashMessage('success')): ?>
   <div class="alert alert-success">
-    <?= Application::$app->session->getFlashMessage('success'); ?>
+    <?= \app\core\Application::$app->session->getFlashMessage('success'); ?>
   </div>
 <?php endif; ?>
-{{content}}
+  {{content}}
+</div>
 <?php require_once(dirname(__DIR__, 1).'/includes/footer.php'); ?>

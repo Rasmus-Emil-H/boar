7d9a95cc3432a294db53a92b1f0ebbbeff83a8e0
troldefar <r_emil@live.dk>
2023-09-10 10:36:14 +0200

    even more annoying error stack trace

--- public/index.php
+++ public/index.php
@@ -5,14 +5,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 use app\core\Application;
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+define('PASSWORD_PASSWORD_DEFAULT', 'qwd');
 
 try {
-    define('PASSWORD_PASSWORD_DEFAULT', 'qwd');
-    require_once dirname(__DIR__) . '/vendor/autoload.php';
     $app = new Application(applicationIsMigrating: false);
     $app->run();
 } catch (\Throwable $e) {
     echo '<div style="background:#e63946;padding:1rem;color:#fff;">
-        '.$e->getMessage() . ' on line: ' . $e->getLine() . ' in file: ' . $e->getFile().'
+        <p>'.$e->getMessage() . '</p> <p> on line: ' . $e->getLine() . '</p> <p> in file: ' . $e->getFile().'</p>
     </div>';
 }

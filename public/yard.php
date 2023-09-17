12bc31dbffb30a520266fe96a7b2e4893a84de1f
troldefar <r_emil@live.dk>
2023-09-17 10:02:28 +0200

    random nonsence i will never ever use but managed to spend to spend 10 minutes on

--- public/yard.php
+++ public/yard.php
@@ -3,16 +3,18 @@
 define('PASSWORD_PASSWORD_DEFAULT', 'qwd');
 define('DEBUG_COLOR', '#e63946');
 
-function dd($input) {
+function displayDD($input): void {
   echo '<pre style="background:'.DEBUG_COLOR.';padding:1rem;opacity:0.'.rand(7, 9).';"> <h2 style="text-align:center;">DEBUGGING</h2> ';
   var_dump($input);
   echo ' <h2 style="text-align:center;">END OF DEBUGGING</h2> </pre>';
+}
+
+function dd($input) {
+  displayDD($input);
   exit;
 }
 
 function d($input) {
-  echo '<pre style="background:'.DEBUG_COLOR.';padding:1rem;opacity:0.'.rand(7, 9).';"> <h2 style="text-align:center;">DEBUGGING</h2> ';
-  var_dump($input);
-  echo ' <h2 style="text-align:center;">END OF DEBUGGING</h2> </pre>';
+  displayDD($input);
   echo '<hr />';
 }

389b75b575b2c648c228906a59109fac4c6e287c
troldefar <r_emil@live.dk>
2023-09-19 06:38:57 +0200

    mv to obj

--- public/resources/js/modules/serviceworkerInstall.js
+++ public/resources/js/modules/serviceworkerInstall.js
@@ -4,74 +4,71 @@ const serviceworkerConfigs = {
     responseError: 408
 };
 
-const addResourcesToCache = async function(resources) {
-    const cache = await caches.open(serviceworkerConfigs.cacheResponseName);
-    await cache.addAll(resources);
-};
-
-const putInCache = async function(request, response) {
-    const cache = await caches.open(serviceworkerConfigs.cacheResponseName);
-    await cache.put(request, response);
-};
-
-const cacheFirst = async function({request, preloadResponsePromise, fallbackUrl}) {
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) return responseFromCache;
-
-    const preloadResponse = await preloadResponsePromise;
-    if (preloadResponse) {
-        putInCache(request, preloadResponse.clone());
-        return preloadResponse;
-    }
-
-    try {
-        const responseFromNetwork = await fetch(request);
-        putInCache(request, responseFromNetwork.clone());
-        return responseFromNetwork;
-    } catch (error) {
-        const fallbackResponse = await caches.match(fallbackUrl);
-        if (fallbackResponse) return fallbackResponse;
-        return new Response(serviceworkerConfigs.networkErrorMessage, {
-            status: serviceworkerConfigs.responseError,
-            headers: {
-                "Content-Type": "text/plain"
-            },
-        });
-    }
-};
-
-const enableNavigationPreload = async function() {
-    if (self.registration.navigationPreload) {
-        await self.registration.navigationPreload.enable();
-    }
-};
-
-const deleteCache = async function(key) {
-    await caches.delete(key);
-};
-
-const deleteOldCaches = async function() {
-    const cacheKeepList = [serviceworkerConfigs.cacheResponseName];
-    const keyList = await caches.keys();
-    const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-    await Promise.all(cachesToDelete.map(deleteCache));
+const serviceWorkerManager = {
+    async addResourcesToCache(resources) {
+        const cache = await caches.open(serviceworkerConfigs.cacheResponseName);
+        await cache.addAll(resources);
+    },
+    async putInCache(request, response) {
+        const cache = await caches.open(serviceworkerConfigs.cacheResponseName);
+        await cache.put(request, response);
+    },
+    async cacheFirst({request, preloadResponsePromise, fallbackUrl}) {
+        const responseFromCache = await caches.match(request);
+        if (responseFromCache) return responseFromCache;
+    
+        const preloadResponse = await preloadResponsePromise;
+        if (preloadResponse) {
+            putInCache(request, preloadResponse.clone());
+            return preloadResponse;
+        }
+    
+        try {
+            const responseFromNetwork = await fetch(request);
+            putInCache(request, responseFromNetwork.clone());
+            return responseFromNetwork;
+        } catch (error) {
+            const fallbackResponse = await caches.match(fallbackUrl);
+            if (fallbackResponse) return fallbackResponse;
+            return new Response(serviceworkerConfigs.networkErrorMessage, {
+                status: serviceworkerConfigs.responseError,
+                headers: {
+                    "Content-Type": "text/plain"
+                },
+            });
+        }
+    },
+    async enableNavigationPreload() {
+        if (self.registration.navigationPreload) {
+            await self.registration.navigationPreload.enable();
+        }
+    },
+    async deleteCache(key) {
+        await caches.delete(key);
+    },
+    async deleteOldCaches() {
+        const cacheKeepList = [serviceworkerConfigs.cacheResponseName];
+        const keyList = await caches.keys();
+        const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+        await Promise.all(cachesToDelete.map(deleteCache));
+    },
 };
 
 self.addEventListener("activate", function(event) {
-    event.waitUntil(deleteOldCaches());
-    event.waitUntil(enableNavigationPreload());
+    event.waitUntil(serviceWorkerManager.deleteOldCaches());
+    event.waitUntil(serviceWorkerManager.enableNavigationPreload());
 });
 
 self.addEventListener("install", function(event) {
     event.waitUntil(
-        addResourcesToCache([
+        serviceWorkerManager.addResourcesToCache([
             "/favicon.ico",
         ]),
     );
 });
 
 self.addEventListener("fetch", function(event) {
-    event.respondWith(cacheFirst({request: event.request, preloadResponsePromise: event.preloadResponse, fallbackUrl: "/favicon.ico"}));
+    event.respondWith(serviceWorkerManager.cacheFirst({request: event.request, preloadResponsePromise: event.preloadResponse, fallbackUrl: "/favicon.ico"}));
 });
 
 self.addEventListener("message", function(event) {

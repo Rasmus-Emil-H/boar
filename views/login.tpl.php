46232afcb5c319ccb2861fd8050d3d79743aaefd
troldefar <r_emil@live.dk>
2023-09-19 06:32:12 +0200

    form

--- views/login.tpl.php
+++ views/login.tpl.php
@@ -1,13 +1,15 @@
 <div class="fullscreen center">
   <div class="screen fade-in">
     <div class="screen__content">
-        <form class="login" method="POST" id="login-form">
-          <div class="login__field">
-              <i class="login__icon fas fa-user"></i>
-              <input type="email" class="login__input" required name="email" placeholder="Email" />
-              <input type="password" class="login__input" required name="password" placeholder="Password" />
+        <form class="login fullscreen center flex-row" method="POST" id="login-form">
+          <div class="flex-row">
+            <div class="login__field">
+                <i class="login__icon fas fa-user"></i>
+                <input type="email" class="login__input" required name="email" placeholder="Email" />
+                <input type="password" class="login__input" required name="password" placeholder="Password" />
+            </div>
+            <button class="button login__submit" id="submit">Login</button>
           </div>
-          <button class="button login__submit" id="submit">Login</button>
           <?= (new app\core\tokens\CsrfToken())->insertHiddenToken(); ?>
         </form>
     </div>

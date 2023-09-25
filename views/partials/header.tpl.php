2810dc1b5f0436d0995d4ac5ac915f2dc9b9a87a
troldefar <r_emil@live.dk>
2023-09-25 16:00:51 +0200

    pwa

--- views/partials/header.tpl.php
+++ views/partials/header.tpl.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Security-Policy" content="script-src 'none'">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="boar application">
     <meta name="msapplication-tap-highlight" content="no">

a670bb73821a953b11c5d693cac5fee9e8eb4f2b
troldefar <r_emil@live.dk>
2023-09-25 21:11:03 +0200

    pwa

--- views/partials/header.tpl.php
+++ views/partials/header.tpl.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Application</title>
+    <title>Boar</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta content="width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=5" name="viewport" />
     <meta name="description" content="boar application">
     <meta name="msapplication-tap-highlight" content="no">
     <link rel="stylesheet" href="/resources/css/bootstrap.min.css">
@@ -15,3 +15,7 @@
     <meta name="theme-color" content="#03045e"/>
   </head>
 <body>
+
+<?php if(!\app\core\Application::isGuest()): ?>
+  <?php require_once $navbar; ?>
+<?php endif; ?>
--- views/partials/navbar.tpl.php
+++ views/partials/navbar.tpl.php
@@ -1,1 +1,29 @@
-navbar
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fa fa-bars text-white"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <a aria-label="logo" class="navbar-brand mt-2 mt-lg-0" href="/">
+        <img src="/resources/images/logo.png" height="50" alt="boar application" loading="lazy" />
+      </a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a aria-label="dashboardbutton" class="nav-link text-white" href="/">Dashboard</a>
+        </li>
+      </ul>
+    </div>
+    <div class="d-flex align-items-center">
+      <div class="dropdown">
+        <a aria-label="dropdown" class="text-reset me-3 dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+          <i class="fas fa-user fa-2x text-white"></i>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+          <li>
+            <a aria-label="logout" class="dropdown-item" href="/auth/logout">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>

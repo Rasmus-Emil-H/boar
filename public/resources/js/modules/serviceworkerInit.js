854135f0206ca8b3a774f369e81c5d0b340198a1
troldefar <r_emil@live.dk>
2023-09-25 07:13:39 +0200

    simplify

--- public/resources/js/modules/serviceworkerInit.js
+++ public/resources/js/modules/serviceworkerInit.js
@@ -3,12 +3,12 @@ let worker = {};
 export default {
     init: function() {
         navigator.serviceWorker.register('/resources/js/modules/serviceworkerInstall.js', {scope: '/'})
-        .then(function(registration) {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(function(error) {
-            console.table('Service Worker registration failed:', error, new Date());
-        });
+            .then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(function(error) {
+                console.table('Service Worker registration failed:', error, new Date());
+            });
     },
     triggerEvent: function(action, resource) {
         if(typeof worker.postMessage === 'function') console.log(worker.postMessage({action, resource}));
@@ -16,9 +16,7 @@ export default {
     unset: function() {
         console.log("Unsetting SW");
         navigator.serviceWorker.getRegistrations().then(function(registrations) {
-            for (let registration of registrations) {
-              registration.unregister();
-            }
+            for (let registration of registrations) registration.unregister();
         });
         console.log("Done unsetting SW");
     }

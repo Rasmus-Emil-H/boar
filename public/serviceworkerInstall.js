409bd41f4ec1203956d9202b56e7d9e684f7a45b
troldefar <r_emil@live.dk>
2023-09-27 10:04:15 +0200

    pwa

--- public/serviceworkerInstall.js
+++ public/serviceworkerInstall.js
@@ -1,12 +1,18 @@
 const cacheName = 'v2';
-const fileCache = 'v2files';
+const fileCache = 'file-cache';
+
+const actions = {
+  message: {
+    CACHE_FILE: 'cache-file',
+    CACHE_PAGE: 'cache-page'
+  }
+};
 
 self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
+
 });
 
 self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
   e.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -19,14 +25,11 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching', e);
   e.respondWith(
     fetch(e.request)
       .then(res => {
         const resClone = res.clone();
-        caches.open(cacheName).then(cache => {
-          cache.put(e.request, resClone);
-        });
+        caches.open(cacheName).then(cache => { cache.put(e.request, resClone); });
         return res;
       })
       .catch(err => caches.match(e.request).then(res => res))
@@ -34,27 +37,28 @@ self.addEventListener('fetch', e => {
 });
 
 self.addEventListener('message', (event) => {
-  if (event.data.action === 'cache-page') {
-    const { url, content } = event.data;
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return fetch(url)
-                .then((response) => {
-                    const resClone = response.clone();
-                    caches.open(cacheName).then(cache => { cache.put(url.request, resClone); });
-                })
-                .catch((e) => {
-                    console.log(e);
-                });
+    if (event.data.action === actions.message.CACHE_PAGE) {
+      const { url } = event.data;
+      event.waitUntil(
+          caches.open(cacheName).then((cache) => {
+              return fetch(url)
+                  .then((response) => {
+                      const resClone = response.clone();
+                      caches.open(cacheName).then(cache => { cache.put(url.request, resClone); });
+                  })
+                  .catch((e) => {
+                      console.log(e);
+                  });
+          })
+      );
+    } else if (event.data.action === actions.message.CACHE_FILE) {
+      const { file } = event.data;
+      console.log(file);
+      event.waitUntil(
+        caches.open(fileCache).then((cache) => {
+          const key = `file-${Date.now()}`;
+          return cache.put(key, new Response(file));
         })
-    );
-  } else if (event.data.action === 'cache-file') {
-    const { blob } = event.data;
-    event.waitUntil(
-      caches.open(fileCache).then((cache) => {
-        const key = `file-${Date.now()}`;
-        return cache.put(key, blob);
-      })
-    );
-  }
+      );
+    }
 });

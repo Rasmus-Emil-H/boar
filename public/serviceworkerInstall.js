b06f4cd07214534a26674e9f38593f96376d37c2
troldefar <r_emil@live.dk>
2023-10-02 21:48:46 +0200

    fd

--- public/serviceworkerInstall.js
+++ public/serviceworkerInstall.js
@@ -19,10 +19,11 @@ const messages = {
 }
 
 self.addEventListener('install', e => {
-
+  e.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
   e.waitUntil(
       caches.keys().then(cacheNames => {
           return Promise.all(

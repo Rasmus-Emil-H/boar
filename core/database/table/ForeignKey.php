8a655f2571b0c1b30a87b1dd20d2d155a54e1cd8
troldefar <r_emil@live.dk>
2023-10-17 07:27:02 +0200

    test

--- core/database/table/ForeignKey.php
+++ core/database/table/ForeignKey.php
@@ -5,10 +5,10 @@ class ForeignKey extends Column {
     protected $foreignTable;
     protected $foreignColumn;
     
-    private const DEFAULT_COLUMN_TYPE = 'int';
+    private const DEFAULT_FOREIGN_KEY_COLUMN_TYPE = 'int';
     
     public function __construct(string $name, string $foreignTable, string $foreignColumn) {
-        parent::__construct($name, self::DEFAULT_COLUMN_TYPE);
+        parent::__construct($name, self::DEFAULT_FOREIGN_KEY_COLUMN_TYPE);
         $this->foreignTable = $foreignTable;
         $this->foreignColumn = $foreignColumn;
     }

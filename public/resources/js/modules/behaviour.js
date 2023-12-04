b7fd61b68d1c468dc821cb15245d862ff48481be
troldefar <r_emil@live.dk>
2023-12-04 20:42:53 +0100

    lim

--- public/resources/js/modules/behaviour.js
+++ public/resources/js/modules/behaviour.js
@@ -1,21 +1,5 @@
 export default {
     init: function() {
-        $('form').on('submit', function(e) {
-            e.preventDefault();
-            $.post(location.href, $(this).serializeArray(), function() {
-
-            });
-        }); 
-    },
-    syncData: function(data) {
-        const body = new FormData();
-        for(let obj in data) body.append(obj, data[obj]);
-        fetch(location.href, { method: 'POST', body })
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                console.log(err);
-            });
+        
     }
 }

c9378f860879cca9a30bf7237e34e1d58fef9108
troldefar <r_emil@live.dk>
2023-12-16 08:34:28 +0100

    permissions table

---
+++ migrations/add_permissions_table_2020_01_02_0001.php
@@ -0,0 +1,22 @@
+<?php
+
+use \app\core\database\table\Table;
+use \app\core\database\Schema;
+
+class add_permissions_table_2020_01_02_0001 {
+
+    public function up() {
+        (new Schema())->up('Permissions', function(Table $table) {
+            $table->increments('PermissionID');
+            $table->varchar('Name', 50);
+            $table->varchar('Readable', 100);
+            $table->timestamp();
+            $table->primaryKey('PermissionID');
+        });
+    }
+
+    public function down() {
+        (new Schema())->down('Permissions');
+    }
+
+}

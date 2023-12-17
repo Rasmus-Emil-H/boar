3b51fb4e94d251e3a1b8d2907eb16bde864c2a39
troldefar <r_emil@live.dk>
2023-12-17 06:19:05 +0100

    debug current querybuilder instance

--- public/yard.php
+++ public/yard.php
@@ -13,7 +13,8 @@
 function displayDD($input, $title = 'Debugging'): void {
   echo '<pre style="padding: 2rem; background-color: #3a6b39; color: white; border-radius: 4px;margin-top: 10px;" class="debug"><h2 class="text-center">' . $title . '</h2><hr>';
   print_r($input);
-  echo '<h2 style="margin:0;padding:0;" class="text-center">End of ' . $title . '</h2></pre>';
+  echo '<hr>';
+  echo '<h2 class="text-center">End of ' . $title . '</h2></pre>';
 }
 
 function dd($input) {

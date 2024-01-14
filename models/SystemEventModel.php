a53d578ae16f3a31f329ba48498b3e029bbc2b15
troldefar <r_emil@live.dk>
2024-01-14 07:36:38 +0100

    aliash push

--- models/SystemEventModel.php
+++ models/SystemEventModel.php
@@ -5,10 +5,6 @@ namespace app\models;
 use \app\core\src\database\Entity;
 
 class SystemEventModel extends Entity {
-
-	public function getAttributes(): array {
-		return [''];
-	}
 	
 	public function getTableName(): string {
 		return 'SystemEvents';
--- models/TranslationModel.php
+++ models/TranslationModel.php
@@ -6,10 +6,6 @@ use \app\core\src\database\Entity;
 
 class TranslationModel extends Entity {
 
-	public function getAttributes(): array {
-		return ['translation'];
-	}
-
 	public function getTableName(): string {
 		return 'Translations';
 	}

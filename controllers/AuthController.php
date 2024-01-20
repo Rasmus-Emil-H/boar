53ddf2f0bfb673fb40eef2b78f57ee987ce4873b
troldefar <r_emil@live.dk>
2024-01-20 08:47:30 +0100

    std alias push

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -16,17 +16,30 @@ class AuthController extends Controller {
         $this->setView('login');
     }
 
+    public function twofactor() {
+        if ($this->request->isGet()) return $this->setView('twofactor');
+        $this->response->setResponse(200, ['SMS SEND']);
+    }
+
     public function logout(): void {
         (new UserModel())->logout();
         $this->session->unset(['user', 'SessionID']);
 		$this->response->redirect('/');
     }
 
+    public function requestNewPassword() {
+        if ($this->request->isGet()) return $this->setView('requestNewPassword');
+    }
+
+    public function validatePasswordResetToken() {
+        $resetToken = $this->requestBody->body->resetPassword ?? false;
+        $resetTokenExists = (new UserModel())->getMetaData()->select()->like(['Data' => 'resetPassword='.$resetToken])->run();
+        if (!$resetToken || empty($resetTokenExists)) $this->response->redirect('/auth/login');
+    }
+
     public function resetPassword() {
         if ($this->request->isGet()) {
-            $resetToken = $this->requestBody->body->resetPassword ?? false;
-            $resetTokenExists = (new UserModel())->getMetaData()->select()->like(['Data' => $resetToken])->run();;
-            if (!$resetToken || empty($resetTokenExists)) $this->response->redirect('/auth/login');
+            $this->validatePasswordResetToken();
             return $this->setView('resetPassword');
         }
         (new UserModel())->resetPassword($this->requestBody->body->email);

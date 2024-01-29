c7473fc4ce0405147c6c36c9f99051ce978e83da
troldefar <r_emil@live.dk>
2024-01-29 07:03:28 +0100

    init appl autho

---
+++ core/src/gate/Gate.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+|----------------------------------------------------------------------------
+| Application authorization
+|----------------------------------------------------------------------------
+| From here you can control access between entities and actions
+| 
+| @author RE_WEB
+| @package \app\core\src\gate
+|
+*/
+
+trait Gate {
+
+    
+
+}

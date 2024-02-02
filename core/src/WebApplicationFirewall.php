60b93eda9700ada7e31ad06a28b7c5ca8a177c30
troldefar <r_emil@live.dk>
2024-02-02 07:02:41 +0100

    std alias push

--- core/src/WebApplicationFirewall.php
+++ core/src/WebApplicationFirewall.php
@@ -5,6 +5,10 @@ namespace app\core\src;
 class WebApplicationFirewall {
     protected $inputData;
 
+    protected const SQL_MESSAGE = 'SQL Injection attempt detected';
+    protected const XSS_MESSAGE = 'XSS attempt detected';
+    protected const DIE_MESSAGE = 'Request blocked: ';
+
     public function __construct() {
         $this->inputData = $_REQUEST;
         $this->sanitizeInput();
@@ -18,18 +22,24 @@ class WebApplicationFirewall {
     }
 
     protected function detectSQLInjection() {
-        foreach ($this->inputData as $value)
-            if (preg_match("/SELECT.*FROM|UNION.*SELECT|DROP.*TABLE|INSERT.*INTO|DELETE.*FROM/i", $value))
-                $this->blockRequest("SQL Injection attempt detected");
+        foreach ($this->inputData as $value) {
+            if (
+                preg_match("/SELECT.*FROM|UNION.*SELECT|DROP.*TABLE|INSERT.*INTO|DELETE.*FROM|ALTER.*TABLE/i", $value) ||
+                preg_match("/\b(AND|OR)\b.+\b(HAVING|FROM|JOIN|INTO|WHERE)\b/i", $value)
+            ) $this->blockRequest(self::SQL_MESSAGE);
+        }
     }
 
     protected function detectXSS() {
-        foreach ($this->inputData as $value)
-            if (preg_match("/<script|<img|onerror|javascript:/i", $value)) 
-                $this->blockRequest("XSS attempt detected");
+        foreach ($this->inputData as $value) {
+            if (
+                preg_match("/<script|<img|onerror|javascript:|document.cookie|eval\(|<iframe/i", $value) ||
+                preg_match("/\b(alert|confirm|prompt)\(/i", $value)
+            ) $this->blockRequest(self::XSS_MESSAGE);
+        }
     }
 
     protected function blockRequest($reason) {
-        die("Request blocked: " . $reason);
+        die(self::DIE_MESSAGE . $reason);
     }
 }

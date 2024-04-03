6e0ccf1276d4cc474f19396b25100aea6df36a09
troldefar <r_emil@live.dk>
2024-04-03 19:53:36 +0200

    std alias push

--- core/src/utilities/Image.php
+++ core/src/utilities/Image.php
@@ -7,8 +7,8 @@ class Image {
     private string $imagePath;
     private string $imageType;
 
-    private const DEFAULT_RESIZE_HEIGHT = 100;
-    private const DEFAULT_RESIZE_WIDTH = 100;
+    private const DEFAULT_RESIZE_HEIGHT = 600;
+    private const DEFAULT_RESIZE_WIDTH = 600;
 
     public function __construct($imagePath) {
         $this->imagePath = $imagePath;
@@ -24,11 +24,28 @@ class Image {
         return $gdImage;
     }
 
+    private function evaluateDimensions(array $originalMeasures): array {
+        list($width, $height) = $originalMeasures;
+
+        return $width > self::DEFAULT_RESIZE_WIDTH || $height > self::DEFAULT_RESIZE_HEIGHT ? $this->calculateAspectRatio($width, $height) : ['width' => $width, 'height' => $height];
+    }
+
+    private function calculateAspectRatio(int $width, int $height): array {
+        if ($width <= self::DEFAULT_RESIZE_WIDTH && $height <= self::DEFAULT_RESIZE_HEIGHT) return ['width' => $width, 'height' => $height];
+        
+        $aspectRatio = $width / $height;
+
+        return self::DEFAULT_RESIZE_WIDTH / $aspectRatio > self::DEFAULT_RESIZE_HEIGHT ? 
+            ['width' => self::DEFAULT_RESIZE_HEIGHT * $aspectRatio, 'height' => self::DEFAULT_RESIZE_HEIGHT] : 
+            ['width' => self::DEFAULT_RESIZE_WIDTH, 'height' => self::DEFAULT_RESIZE_WIDTH / $aspectRatio];
+    }
+
     public function resizeImage($width = self::DEFAULT_RESIZE_WIDTH, $height = self::DEFAULT_RESIZE_HEIGHT): bool {
         $imageInfo = getimagesize($this->imagePath);
         $this->imageType = $imageInfo[2];
         $gdImage = $this->imageCreateFrom();
-        $resized = imagescale($gdImage, $width, $height);
+        $dimensions = $this->evaluateDimensions($imageInfo);
+        $resized = imagescale($gdImage, (int)$dimensions['width'], (int)$dimensions['height']);
 
         imagejpeg($resized, $this->imagePath);
         imagedestroy($gdImage);

89fbddf53f8dbb7d4911a7b69556175dc6da0487
troldefar <r_emil@live.dk>
2024-05-16 15:50:54 +0200

    std alias push

--- core/src/database/querybuilder/QueryBuilder.php
+++ core/src/database/querybuilder/QueryBuilder.php
@@ -252,8 +252,41 @@ class QueryBuilder extends QueryBuilderBase {
         return $this;
     }
 
+    public function distinct(): self {
+        $this->upsertQuery('SELECT DISTINCT ' . $this->fields . ' FROM ' . $this->table);
+        return $this;
+    }
+
     public function as(string $as): self {
         $this->upsertQuery('as ' . $as . ' ');
+        return $this;
+    }
+
+    public function having(array $conditions): self {
+        foreach ($conditions as $field => $value) {
+            $this->upsertQuery("HAVING {$field} = :{$field}");
+            $this->updateQueryArguments($field, $value);
+        }
+        return $this;
+    }
+
+    public function sum(string $field, string $alias = 'sum'): self {
+        $this->upsertQuery("SELECT SUM({$field}) AS {$alias} FROM {$this->table}");
+        return $this;
+    }
+    
+    public function avg(string $field, string $alias = 'avg'): self {
+        $this->upsertQuery("SELECT AVG({$field}) AS {$alias} FROM {$this->table}");
+        return $this;
+    }
+    
+    public function min(string $field, string $alias = 'min'): self {
+        $this->upsertQuery("SELECT MIN({$field}) AS {$alias} FROM {$this->table}");
+        return $this;
+    }
+    
+    public function max(string $field, string $alias = 'max'): self {
+        $this->upsertQuery("SELECT MAX({$field}) AS {$alias} FROM {$this->table}");
         return $this;
     }
 

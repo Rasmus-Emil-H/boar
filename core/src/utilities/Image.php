bc216b8c9c0f0f1d4d1e3a8330abf2756d7e29c1
troldefar <r_emil@live.dk>
2024-05-17 08:53:16 +0200

    std alias push

--- core/src/utilities/Image.php
+++ core/src/utilities/Image.php
@@ -5,20 +5,19 @@ namespace app\core\src\utilities;
 class Image {
 
     private string $imagePath;
-    private string $imageType;
+    private int $imageType;
 
-    private const DEFAULT_RESIZE_HEIGHT = 1000;
-    private const DEFAULT_RESIZE_WIDTH = 1000;
+    private const DEFAULT_RESIZE_HEIGHT = 2000;
+    private const DEFAULT_RESIZE_WIDTH = 2000;
 
     private const WIDTH = 'width'; 
-    private const HEIGHT = 'width'; 
+    private const HEIGHT = 'height';
 
     public function __construct(string $imagePath) {
         $this->imagePath = $imagePath;
     }
 
-    public function imageCreateFrom(): ?\GdImage {
-
+    private function imageCreateFrom(): ?\GdImage {
         $gdImage = null;
         if ($this->imageType == IMAGETYPE_JPEG) $gdImage = imagecreatefromjpeg($this->imagePath);
         if ($this->imageType == IMAGETYPE_PNG)  $gdImage = imagecreatefrompng($this->imagePath);
@@ -30,30 +29,60 @@ class Image {
     private function evaluateDimensions(array $originalMeasures): array {
         list($width, $height) = $originalMeasures;
 
-        return $width > self::DEFAULT_RESIZE_WIDTH || $height > self::DEFAULT_RESIZE_HEIGHT ? $this->calculateAspectRatio($width, $height) : [self::WIDTH => $width, self::HEIGHT => $height];
+        return ($width > self::DEFAULT_RESIZE_WIDTH || $height > self::DEFAULT_RESIZE_HEIGHT) 
+            ? $this->calculateAspectRatio($width, $height) 
+            : [self::WIDTH => $width, self::HEIGHT => $height];
     }
 
     private function calculateAspectRatio(int $width, int $height): array {
-        if ($width <= self::DEFAULT_RESIZE_WIDTH && $height <= self::DEFAULT_RESIZE_HEIGHT) return [self::WIDTH => $width, self::HEIGHT => $height];
-        
+        if ($width <= self::DEFAULT_RESIZE_WIDTH && $height <= self::DEFAULT_RESIZE_HEIGHT)
+            return [self::WIDTH => $width, self::HEIGHT => $height];
+
         $aspectRatio = $width / $height;
 
-        return self::DEFAULT_RESIZE_WIDTH / $aspectRatio > self::DEFAULT_RESIZE_HEIGHT ? 
-            [self::WIDTH => self::DEFAULT_RESIZE_HEIGHT * $aspectRatio, self::HEIGHT => self::DEFAULT_RESIZE_HEIGHT] : 
-            [self::WIDTH => self::DEFAULT_RESIZE_WIDTH, self::HEIGHT => self::DEFAULT_RESIZE_WIDTH / $aspectRatio];
+        return $width / $height > self::DEFAULT_RESIZE_WIDTH / self::DEFAULT_RESIZE_HEIGHT ? 
+        [self::WIDTH => self::DEFAULT_RESIZE_WIDTH, self::HEIGHT => self::DEFAULT_RESIZE_WIDTH / $aspectRatio] :
+        [self::WIDTH => self::DEFAULT_RESIZE_HEIGHT * $aspectRatio, self::HEIGHT => self::DEFAULT_RESIZE_HEIGHT];
     }
 
-    public function resizeImage($width = self::DEFAULT_RESIZE_WIDTH, $height = self::DEFAULT_RESIZE_HEIGHT): bool {
+    public function resizeImage(int $newWidth = self::DEFAULT_RESIZE_WIDTH, int $newHeight = self::DEFAULT_RESIZE_HEIGHT): bool {
         $imageInfo = getimagesize($this->imagePath);
+        if ($imageInfo === false)
+            return false;
+
         $this->imageType = $imageInfo[2];
         $gdImage = $this->imageCreateFrom();
-        $dimensions = $this->evaluateDimensions($imageInfo);
-        $resized = imagescale($gdImage, (int)$dimensions[self::WIDTH], (int)$dimensions[self::HEIGHT]);
 
-        imagejpeg($resized, $this->imagePath);
+        if ($gdImage === null)
+            return false;
+
+        $dimensions = $this->evaluateDimensions([$newWidth, $newHeight]);
+        $resized = imagescale($gdImage, $dimensions[self::WIDTH], $dimensions[self::HEIGHT], IMG_BILINEAR_FIXED);
+
+        if ($resized === false) {
+            imagedestroy($gdImage);
+            return false;
+        }
+
+        $success = false;
+
+        switch ($this->imageType) {
+            case IMAGETYPE_JPEG:
+                $success = imagejpeg($resized, $this->imagePath);
+                break;
+            case IMAGETYPE_PNG:
+                $success = imagepng($resized, $this->imagePath);
+                break;
+            case IMAGETYPE_GIF:
+                $success = imagegif($resized, $this->imagePath);
+                break;
+            default:
+                return false;
+        }
+
         imagedestroy($gdImage);
         imagedestroy($resized);
 
-        return true;
+        return $success;
     }
 }

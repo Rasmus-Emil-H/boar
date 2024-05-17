1ef9ec51dda9a99968e6193d0cf695e030fea281
troldefar <r_emil@live.dk>
2024-05-17 07:51:24 +0200

    std alias push

--- core/src/utilities/Image.php
+++ core/src/utilities/Image.php
@@ -7,8 +7,11 @@ class Image {
     private string $imagePath;
     private string $imageType;
 
-    private const DEFAULT_RESIZE_HEIGHT = 600;
-    private const DEFAULT_RESIZE_WIDTH = 600;
+    private const DEFAULT_RESIZE_HEIGHT = 1000;
+    private const DEFAULT_RESIZE_WIDTH = 1000;
+
+    private const WIDTH = 'width'; 
+    private const HEIGHT = 'width'; 
 
     public function __construct($imagePath) {
         $this->imagePath = $imagePath;
@@ -27,17 +30,17 @@ class Image {
     private function evaluateDimensions(array $originalMeasures): array {
         list($width, $height) = $originalMeasures;
 
-        return $width > self::DEFAULT_RESIZE_WIDTH || $height > self::DEFAULT_RESIZE_HEIGHT ? $this->calculateAspectRatio($width, $height) : ['width' => $width, 'height' => $height];
+        return $width > self::DEFAULT_RESIZE_WIDTH || $height > self::DEFAULT_RESIZE_HEIGHT ? $this->calculateAspectRatio($width, $height) : [self::WIDTH => $width, self::HEIGHT => $height];
     }
 
     private function calculateAspectRatio(int $width, int $height): array {
-        if ($width <= self::DEFAULT_RESIZE_WIDTH && $height <= self::DEFAULT_RESIZE_HEIGHT) return ['width' => $width, 'height' => $height];
+        if ($width <= self::DEFAULT_RESIZE_WIDTH && $height <= self::DEFAULT_RESIZE_HEIGHT) return [self::WIDTH => $width, self::HEIGHT => $height];
         
         $aspectRatio = $width / $height;
 
         return self::DEFAULT_RESIZE_WIDTH / $aspectRatio > self::DEFAULT_RESIZE_HEIGHT ? 
-            ['width' => self::DEFAULT_RESIZE_HEIGHT * $aspectRatio, 'height' => self::DEFAULT_RESIZE_HEIGHT] : 
-            ['width' => self::DEFAULT_RESIZE_WIDTH, 'height' => self::DEFAULT_RESIZE_WIDTH / $aspectRatio];
+            [self::WIDTH => self::DEFAULT_RESIZE_HEIGHT * $aspectRatio, self::HEIGHT => self::DEFAULT_RESIZE_HEIGHT] : 
+            [self::WIDTH => self::DEFAULT_RESIZE_WIDTH, self::HEIGHT => self::DEFAULT_RESIZE_WIDTH / $aspectRatio];
     }
 
     public function resizeImage($width = self::DEFAULT_RESIZE_WIDTH, $height = self::DEFAULT_RESIZE_HEIGHT): bool {
@@ -45,7 +48,7 @@ class Image {
         $this->imageType = $imageInfo[2];
         $gdImage = $this->imageCreateFrom();
         $dimensions = $this->evaluateDimensions($imageInfo);
-        $resized = imagescale($gdImage, (int)$dimensions['width'], (int)$dimensions['height']);
+        $resized = imagescale($gdImage, (int)$dimensions[self::WIDTH], (int)$dimensions[self::HEIGHT]);
 
         imagejpeg($resized, $this->imagePath);
         imagedestroy($gdImage);

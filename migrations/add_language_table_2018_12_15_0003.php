9544e188294eea296eb5f31542c4ff5f46a9477b
troldefar <r_emil@live.dk>
2024-05-22 13:53:02 +0200

    std alias push

--- migrations/add_language_table_2018_12_15_0003.php
+++ migrations/add_language_table_2018_12_15_0003.php
@@ -8,8 +8,8 @@ class add_language_table_2018_12_15_0003 {
     public function up() {
         (new Schema())->up('Languages', function(Table $table) {
             $table->increments('LanguageID');
-            $table->varchar('Name', 10);
-            $table->varchar('Code', 2);
+            $table->varchar('Name', 20);
+            $table->varchar('Code', 3);
             $table->timestamp('CreatedAt');
             $table->primaryKey('LanguageID');
         });

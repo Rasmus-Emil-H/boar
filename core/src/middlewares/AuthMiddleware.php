2738b4f2710938575be2178fd37880e026a6c713
troldefar <r_emil@live.dk>
2024-05-22 12:54:32 +0200

    std alias push

--- core/src/middlewares/AuthMiddleware.php
+++ core/src/middlewares/AuthMiddleware.php
@@ -9,7 +9,6 @@
 namespace app\core\src\middlewares;
 
 use \app\core\src\exceptions\ForbiddenException;
-use \app\core\src\miscellaneous\CoreFunctions;
 
 class AuthMiddleware extends Middleware {
 
@@ -22,7 +21,7 @@ class AuthMiddleware extends Middleware {
     public function execute() {
         if (app()::isGuest()) 
             if (empty($this->actions) || in_array(app()::$app->controller->action, $this->actions)) 
-                throw new ForbiddenException();
+                app()->getResponse()->redirect('/');
     }
 
 }

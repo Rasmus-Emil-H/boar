2c4cd09c728aa4f355ec90346401075b4022484f
troldefar <r_emil@live.dk>
2024-05-30 13:17:47 +0200

    std alias push

--- core/src/miscellaneous/Hash.php
+++ core/src/miscellaneous/Hash.php
@@ -4,7 +4,9 @@ namespace app\core\src\miscellaneous;
 
 class Hash {
 
-    public static function create(int $length = 50): string {
+    protected const DEFAULT_HASH_LENGTH = 50;
+
+    public static function create(int $length = self::DEFAULT_HASH_LENGTH): string {
         $randomBytes  = random_bytes($length);
         $uniqueString = base64_encode($randomBytes);
         $uniqueString = substr(preg_replace("/[^a-zA-Z0-9]/", "", $uniqueString), 0, $length);
@@ -13,6 +15,12 @@ class Hash {
 
     public static function uuid(): string {
         return hash('sha256', uniqid());
+    }
+
+    public static function createdBasedOn(string $base, int $length = self::DEFAULT_HASH_LENGTH) {
+        $b64 = base64_encode($base);
+        $uniqueString = substr(preg_replace("/[^a-zA-Z0-9]/", "", $b64), 0, $length);
+        return $uniqueString;
     }
 
 }

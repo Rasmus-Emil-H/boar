66e8ac8ed373c26aab7ca00bbf6414180699fbd1
troldefar <r_emil@live.dk>
2024-06-21 08:30:31 +0200

    push function

--- public/resources/js/modules/pushapi.js
+++ public/resources/js/modules/pushapi.js
@@ -2,9 +2,12 @@ export default {
     init: function() {
         if (('PushManager' in window)) return;
     
-        window.autologik.components.toast(
+        window.boar.components.toast(
             'Push API is not avaliable on your device', 
             window.boar.constants.mdbootstrap.ERROR_CLASS
         );
+    },
+    sendMessage: function(registration, body, tag, icon = "/resources/images/logo.png") {
+        registration.showNotification("Boar push message", {body, icon, vibrate: [200, 100, 200, 100, 200, 100, 200], tag});
     }
 }
--- public/serviceworkerInit.js
+++ public/serviceworkerInit.js
@@ -3,12 +3,8 @@ let worker = {};
 export default {
     init: function() {
         navigator.serviceWorker.register('/serviceworkerInstall.js', {scope: '/'})
-            .then(function(registration) {
-                
-            })
-            .catch(function(error) {
-                
-            });
+            .then(function(registration) {})
+            .catch(function(error) {});
     },
     triggerEvent: function(action, resource) {
         if(typeof worker.postMessage === 'function') console.log(worker.postMessage({action, resource}));
@@ -17,5 +13,9 @@ export default {
         navigator.serviceWorker.getRegistrations().then(function(registrations) {
             for (let registration of registrations) registration.unregister();
         });
+    },
+    dispatchPushNotification: async function(body, icon = '/resources/images/logo.png', tag) {
+        const registration = await navigator.serviceWorker.getRegistration();
+        registration.showNotification("Boar", {body, icon, vibrate: [200, 100, 200, 100, 200, 100, 200], tag});
     }
 }

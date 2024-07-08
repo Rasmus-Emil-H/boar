4b6456db50be6e54b11bdc2cd4fd5f1c4d1560ee
troldefar <r_emil@live.dk>
2024-07-08 14:50:49 +0200

    std alias push

--- public/resources/js/modules/notifications.js
+++ public/resources/js/modules/notifications.js
@@ -3,7 +3,7 @@ export default {
         const permission = await Notification.requestPermission();
         const perm = permission !== 'granted' ? 'not granted' : 'granted';
 
-        window.autologik.components.toast(
+        window.boar.components.toast(
             `Notification permission ${perm}`, 
             window.boar.constants.mdbootstrap[perm === 'granted' ? 'SUCCESS_CLASS' : 'ERROR_CLASS']
         );

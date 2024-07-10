76265b006ca465950cb4cdb1177358db34c5f3ec
troldefar <r_emil@live.dk>
2024-07-10 12:32:05 +0200

    std alias push

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -9,12 +9,11 @@
 namespace app\controllers;
 
 use \app\core\src\Controller;
-use \app\core\src\middlewares\AuthMiddleware;
 
 class HomeController extends Controller {
 
     public function index() {
-        $this->setFrontendTemplateAndData(templateFile: 'home');
+        $this->setFrontendTemplateAndData('home', ['boar' => 'is live and running']);
     }
 
 }

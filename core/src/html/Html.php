de7af15b5239b268048abc771ed21df32ce103cb
troldefar <r_emil@live.dk>
2024-07-11 09:55:21 +0200

    std alias push

--- core/src/html/Html.php
+++ core/src/html/Html.php
@@ -16,12 +16,12 @@ namespace app\core\src\html;
 
 class Html {
 
-    public static function datePicker($value, $name, $placeholder): string {
+    public static function datePicker(string $value, string $name, string $placeholder): string {
         ob_start(); ?>
-            <div data-mdb-start-day="1" class="form-outline" data-mdb-datepicker-init data-mdb-input-init data-mdb-inline="true" data-mdb-format="dd-mm-yyyy" >
-                <input value="<?= hs($value ?? ''); ?>" name="<?= hs($name); ?>" type="text" class="form-control" id="datetimepickerExample" placeholder="<?= hs($placeholder); ?>" />
+            <div data-mdb-start-day="1" class="form-outline" data-mdb-datepicker-init data-mdb-input-init data-mdb-inline="true" data-mdb-format="dd-mm-yyyy">
+                <input data-inject-data-type="" value="<?= hs($value ?? ''); ?>" name="<?= hs($name); ?>" type="text" class="form-control" id="datetimepickerExample" placeholder="<?= hs($placeholder); ?>" />
             </div>
         <?php return ob_get_clean();
-    }    
+    }
 
 }

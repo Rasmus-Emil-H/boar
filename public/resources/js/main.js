70ab363b117aecd2c1a7a4b6bb254ec0de22986e
troldefar <r_emil@live.dk>
2024-07-12 08:12:12 +0200

    std alias push

--- public/resources/js/main.js
+++ public/resources/js/main.js
@@ -9,13 +9,15 @@
 
 const version = '?t=1';
 const modulesDir = './modules/';
+const jsExtension = '.js';
+
 const modules = ['utilities', 'components', 'behaviour', 'constants', 'websocket'];
 const allModules = modules.map(function(module) {
-  return `${modulesDir}${module}.js${version}`;
+  return `${modulesDir}${module}${jsExtension}${version}`;
 });
 
 const modulesToImport = [
-  `../../serviceworkerInit.js${version}`,
+  `../../serviceworkerInit${jsExtension}${version}`,
   ...allModules
 ];
 
@@ -26,7 +28,7 @@ document.addEventListener("DOMContentLoaded", async function() {
     for (const modulePath of modulesToImport) {
         try {
             const module = await import(modulePath);
-            const moduleName = modulePath.split('/').pop().replace(`.js${version}`, '');
+            const moduleName = modulePath.split('/').pop().replace(`${jsExtension}${version}`, '');
             window.boar[moduleName] = module.default;
             if (modulePath.includes('websocket')) continue;
             Object.freeze(window.boar[moduleName]);

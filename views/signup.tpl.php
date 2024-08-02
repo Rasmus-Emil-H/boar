3dc8ef003389226eff9f86c6332a928f3a6dfc07
troldefar <r_emil@live.dk>
2024-08-02 18:41:04 +0200

    std alias push

--- views/signup.tpl.php
+++ views/signup.tpl.php
@@ -13,8 +13,8 @@
     <div class="form-group w-100 mb-3">
       <input type="password" required name="password" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1">
     </div>
-    <?= (new \app\core\src\tokens\CsrfToken())->insertHiddenToken(); ?>
-    <button type="submit" class="btn btn-success btn-lg mt-2 w-100"><?= \app\core\src\miscellaneous\CoreFunctions::ths('Create account'); ?></button>
-    <a href="/" class="btn btn-info btn-lg mt-2 w-100"><?= \app\core\src\miscellaneous\CoreFunctions::ths('Go back'); ?></a>
+    <?= CSRFTokenInput(); ?>
+    <button type="submit" class="btn btn-success btn-lg mt-2 w-100"><?= ths('Create account'); ?></button>
+    <a href="/" class="btn btn-info btn-lg mt-2 w-100"><?= ths('Go back'); ?></a>
   </form>
 </div>

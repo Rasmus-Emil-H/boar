f1f3ebc5372c0f2e7fca33ba5d68617d64f64208
troldefar <r_emil@live.dk>
2024-08-02 18:48:29 +0200

    std alias push

--- views/playground.tpl.php
+++ views/playground.tpl.php
@@ -3,7 +3,7 @@
         <div class="request">
             <div class="form-floating mx-auto my-auto">
                 <form method="POST">
-                    <textarea name="Input" class="w-100 border-0 vh50 noresize p-2" placeholder="echo 123;" id="floatingTextarea2"></textarea>
+                    <textarea name="Input" class="w-100 rounded vh50 noresize p-2" placeholder="echo 123;" id="floatingTextarea2"></textarea>
                     <button type="submit" id="php" class="btn btn-success w-100">Debug</button>
                     <?= CSRFTokenInput(); ?>
                 </form>
@@ -11,9 +11,9 @@
         </div>
     </div>
 
-    <div class="receive w-75 mx-auto">
+    <div class="receive w-75 mx-auto mt-2">
         <div class="form-floating mx-auto my-auto" style="margin-left:2px !important;">
-            <textarea name="Input" class="w-100 border-0 vh50 noresize p-2 bg-white" disabled id="inject"></textarea>
+            <textarea name="Input" class="w-100 rounded vh50 noresize p-2 bg-white" disabled id="inject"></textarea>
         </div>
     </div>
 </div>

faa8fce4180d9d179d88d81259058a2823e1676c
troldefar <r_emil@live.dk>
2024-08-02 18:57:51 +0200

    std alias push

--- views/partials/navbar.tpl.php
+++ views/partials/navbar.tpl.php
@@ -1,15 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark text-white">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/home"><?= app()->getConfig()->get('appName'); ?></a>
+        <a class="navbar-brand" href="/home"><i class="bi bi-house"></i></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav float-end">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <?= ths('Menu'); ?>
-                    </a>
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-list"></i></a>
                     <ul class="dropdown-menu">
                         <?php \app\core\src\miscellaneous\CoreFunctions::loopAndEcho($navigationItems); ?>
                     </ul>

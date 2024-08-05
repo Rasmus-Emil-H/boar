30adf6dfe36462d2a1a83ef2f10f175314374040
troldefar <r_emil@live.dk>
2024-08-05 11:22:28 +0200

    std alias push

--- views/error.tpl.php
+++ views/error.tpl.php
@@ -5,7 +5,7 @@
         <h1 class="mb-4"><?= $exception->getCode(); ?></h1>
         <hr class="w-50" />
         <h4 class="mb-4 mt-4"><?= app()::isDevSite() ? $exception->getMessage() : ths('Error'); ?></h4>
-        <a href="<?= hs($home); ?>" class="w-50 p-2 btn btn-success"><?= ths('GO HOME'); ?></a>
+        <a href="<?= hs($home); ?>" class="p-2 btn btn-success"><?= ths('GO HOME'); ?></a>
     </div>
     <div class="image"></div>
 </div>

1d81ca1256666d526f89e1b57779f20913f41487
troldefar <r_emil@live.dk>
2024-08-05 06:59:15 +0200

    std alias push

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -8,10 +8,11 @@ class ErrorController extends Controller {
 
     public function index($error) {
         $this->response->setStatusCode($error->getCode());
-        $this->setView('error');
-        $this->setData(['exception' => $error]);
+
         $this->addStylesheet('error');
+        $this->setFrontendTemplateAndData('error', ['exception' => $error]);
         extract($this->getData());
+        
         require_once $this->getView();
     }
 

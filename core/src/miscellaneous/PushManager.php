5fd0ef2e3286ec23c5c33707796083fca2f0c46a
troldefar <r_emil@live.dk>
2024-08-19 07:55:03 +0200

    std alias push

--- core/src/miscellaneous/PushManager.php
+++ core/src/miscellaneous/PushManager.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+|----------------------------------------------------------------------------
+| Default Push manager
+|----------------------------------------------------------------------------
+|
+| Keys can be generated via
+|
+| openssl ecparam -genkey -name prime256v1 -out private_key.pem
+| openssl ec -in private_key.pem -pubout -outform DER|tail -c 65|base64|tr -d '=' |tr '/+' '_-' >> public_key.txt
+| openssl ec -in private_key.pem -outform DER|tail -c +8|head -c 32|base64|tr -d '=' |tr '/+' '_-' >> private_key.txt
+| 
+| @author RE_WEB
+| @package \app\core\src
+|
+*/
+
 namespace app\core\src\miscellaneous;
 
 use app\core\src\http\Curl;

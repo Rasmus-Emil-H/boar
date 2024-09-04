88cf6ad68b33818f338aa8fcd1cf7821a95793bb
troldefar <r_emil@live.dk>
2024-09-04 14:50:16 +0200

    std alias push

--- migrations/edit_sessions_table_add_user_id_and_ip_2019_01_10_0001.php
+++ migrations/edit_sessions_table_add_user_id_and_ip_2019_01_10_0001.php
@@ -8,11 +8,12 @@ class edit_sessions_table_add_user_id_and_ip_2019_01_10_0001 {
     public function up() {
         (new Schema())->table('Sessions', function(Table $table) {
             $table->integer('UserID')->add();
+            $table->varchar('IP', 20)->add();
         });
     }
 
     public function down() {
-        (new Schema())->table('Meta', function(Table $table) {
+        (new Schema())->table('Sessions', function(Table $table) {
             $table->dropColumns(['UserID', 'IP']);
         });
     }

d66a88a0ffbc061a6aac6692e7415eda0e3198ed
RASMUS HANSEN <rasmus@sharksmedia.dk>
2024-11-08 09:29:47 +0100

    std alias push

--- public/resources/js/workers/implementations/std.js
+++ public/resources/js/workers/implementations/std.js
@@ -1,9 +1,11 @@
-onmessage = async(e) => {
+onmessage = async (e) => {
     try {
         const fd = new FormData();
         for (let key in e.data) fd.append(key, e.data[key]);
+
         const response = await fetch(e.data.url ?? '/file', { method: "POST", body: fd });
         const jsonResponse = await response.json();
+        
         postMessage(jsonResponse.responseJSON ?? '');
     } catch (e) {
         console.log(e);

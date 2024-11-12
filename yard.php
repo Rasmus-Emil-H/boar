7594c096bbe0247f2a03efa568aa1745159f4564
troldefar <r_emil@live.dk>
2024-11-12 06:51:49 +0100

    std alias push

--- yard.php
+++ yard.php
@@ -79,15 +79,3 @@ function renderComponent($method, $arguments = []) {
 function CSRFTokenInput(): string {
     return (new \app\core\src\tokens\CsrfToken())->insertHiddenToken();
 }
-
-/**
- * Dump and die
- */
-
- function dumpAndDie(mixed $input) {
-    return \app\core\src\miscellaneous\CoreFunctions::dd($input);
-}
-
-function panic(string $reason = ''): void {
-    exit($reason);
-}

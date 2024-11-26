3658bd4589925ea10798efe3e8d2b5a9bec31038
troldefar <r_emil@live.dk>
2024-11-26 13:22:34 +0100

    std alias push

--- views/error.tpl.php
+++ views/error.tpl.php
@@ -2,7 +2,11 @@
 
 <div class="fullscreen error-template">
     <div class="error fullscreen">
-        <h1 class="mb-4"><?= $exception->getCode(); ?></h1>
+        <h1 class="mb-4">
+            ⚒️
+            <?= $exception->getCode(); ?>
+            👨🏼‍🔧
+        </h1>
         <hr class="w-50" />
         <h4 class="mb-4 mt-4"><?= app()::isDevSite() ? $exception->getMessage() : ths('Error'); ?></h4>
         <a href="<?= hs($home); ?>" class="p-2 btn btn-success"><?= ths('GO HOME'); ?></a>

6db7f78e41ce8c8f7e9dacf8e64c9795496608e3
troldefar <r_emil@live.dk>
2024-12-10 06:14:10 +0100

    moar adapters.. moar

---
+++ core/src/database/adapters/SQLLite.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+|----------------------------------------------------------------------------
+| MySQL adapter
+|----------------------------------------------------------------------------
+|
+| @author RE_WEB
+| @package core
+|
+*/
+
+
+namespace app\core\src\database\adapters;
+
+use PDO;
+
+class MySQL extends Adapter {
+
+    private array $options = [
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+        \PDO::ATTR_EMULATE_PREPARES => false,
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+    ];
+
+    protected string $driverName = 'sqlite';
+
+    public function doConnect(object $config): PDO {
+        $pdo = new PDO($this->getDriverName() . ':' . $config->dsn, $config->user, $config->password, $this->options);
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }
+
+}

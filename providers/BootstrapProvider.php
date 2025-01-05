23e385136711ca090348aaa80e7eb57f94b53e31
troldefar <r_emil@live.dk>
2025-01-05 13:41:44 +0100

    initial test of service provider setup

--- providers/BootstrapProvider.php
+++ providers/BootstrapProvider.php
@@ -2,17 +2,13 @@
 
 namespace app\providers;
 
-use \app\core\Application;
 use \app\core\src\providers\ServiceProvider;
 use \app\services\WeatherAPI;
 
 class BootstrapProvider implements ServiceProvider {
 
     public function register(): void {
-        app()->getServiceProvider()->bind(WeatherAPI::class, function(Application $app) {
-            var_dump($app);
-            // return $app->setGlobalService(new WeatherAPI());
-        });
+        app()->getServiceProvider()->bind(WeatherAPI::class);
     }
 
 }

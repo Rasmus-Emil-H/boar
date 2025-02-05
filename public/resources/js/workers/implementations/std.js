a617c78d35c18f9e54a0f42aa0bec613ccf099bf
troldefar <r_emil@live.dk>
2025-02-05 20:40:21 +0100

    [] from forms

--- public/resources/js/workers/implementations/std.js
+++ public/resources/js/workers/implementations/std.js
@@ -1,6 +1,12 @@
 onmessage = async(e) => {
     const fd = new FormData();
-    for (let key in e.data) fd.append(key, e.data[key]);
+    for (let key in e.data) {
+        if (typeof e.data[key] === 'object') {
+            for (let iteration in e.data[key])
+                fd.append(`${key.replace('[]', '')}[]`, e.data[key][iteration]);
+        } else fd.append(key, e.data[key])
+    };
+
     const response = await fetch(e.data.url ?? '/file', {method: "POST", body: fd});
     const jsonResponse = await response.json();
     postMessage(jsonResponse.responseJSON ?? '');
--- public/resources/js/workers/main.js
+++ public/resources/js/workers/main.js
@@ -2,6 +2,9 @@ import NotificationHandler from './NotificationHandler.js';
 import WorkerManager from './WorkerManager.js';
 
 class WorkerParent {
+    
+    MULTIPLE_INPUT_INDICATOR = '[]';
+
     constructor(options) {
         this.options = options;
 
@@ -31,7 +34,19 @@ class WorkerParent {
     prepareData() {
         const body = {};
         
-        for (let [key, value] of this.data.entries()) body[key] = value;
+        for (let [key, value] of this.data.entries()) {
+            if (!key.includes(this.MULTIPLE_INPUT_INDICATOR)) {
+                body[key] = value;
+                continue;
+            }
+
+            const replacedKey = key.replace('[]', '');
+
+            if (!body[replacedKey]) body[replacedKey] = [];
+
+            body[replacedKey].push(value);
+        }
+
         if (!Object.keys(body).includes('url')) body.url = location.href;
 
         return body;
@@ -42,7 +57,7 @@ class WorkerParent {
 
         this.workerManager.setOnMessage((e) => {
             this.notificationHandler.showNotification();
-            if (!this.callback) return; 
+            if (!this.callback) return;
             
             this.callback(this.target, { responseJSON: e.data, additionalData: this.additionalData });
         });

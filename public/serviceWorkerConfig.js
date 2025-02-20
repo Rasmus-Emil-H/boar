157340e7d287be59357ed0e8c13877975aa9f0f1
troldefar <r_emil@live.dk>
2025-02-20 11:53:46 +0100

    couch commit

--- public/serviceWorkerConfig.js
+++ public/serviceWorkerConfig.js
@@ -1,6 +1,6 @@
 import IndexedDBManager from '/resources/js/modules/indexedDB.js';
 
-const config = {
+const config = {    
     caches: {
         GETCache: 'GETCache',
         POSTCache: 'POSTCache',
@@ -17,7 +17,7 @@ const config = {
             const proceed = !config.externalResources.some(item => new RegExp(`/${item}/`).test(e.request.url));
             if (!proceed) return proceed;
             
-            if (e.request.url === config.psudo.login && e.request.method === 'POST' && !online() || e.request.url.includes('/push')) return false;
+            if (e.request.url === config.psudo.login && e.request.method === 'POST' && !navigator.onLine || e.request.url.includes('/push')) return false;
             if (!config.request.validMethods.includes(e.request.method)) return false;
 
             return true;
@@ -26,23 +26,32 @@ const config = {
             try {
                 const cache = await caches.open(config.caches.GETCache);
                 const cachedResponse = await cache.match(request);
-                
+
+                const {quota, usage} = await navigator.storage.estimate();
+                const freeSpace = quota - usage > 0;
+
                 if (cachedResponse) {
-                    fetch(request).then(async (networkResponse) => {
-                        if (networkResponse.ok && !networkResponse.redirected) 
-                            await cache.put(request, networkResponse.clone());
-                    });
+                    try {
+                        if (navigator?.onLine) {
+                            fetch(request).then(async (networkResponse) => {
+                                if (networkResponse.ok && !networkResponse.redirected) 
+                                    await cache.put(request, networkResponse.clone());
+                            });
+                        }
+                    } catch(e) {
+                        return cachedResponse;
+                    }
 
                     return cachedResponse;
                 }
-                
+
                 const networkResponse = await fetch(request);
-                if (networkResponse.ok && !networkResponse.redirected) 
-                    await cache.put(request, networkResponse.clone());
+                if (networkResponse.ok && !networkResponse.redirected && freeSpace) await cache.put(request, networkResponse.clone());
 
                 return networkResponse;
             } catch (error) {
-                return new Response('Network error', { status: 500 });
+                console.log(error);
+                return new Response('Network error', { status: 200 });
             }
         },
         POST: async function(request) {
@@ -55,12 +64,16 @@ const config = {
             try {
                 const response = await fetch(clonedRequest.url, {mode: 'cors', method: 'POST', body: formDataToSend});
 
-                if (!response.ok || !config.psudo.qualifiedRequestResponsesCode.includes(response.status)) config.buildIndexDBRecord(request);
-                else config.evictGETVersion(request.url);
+                if (response.status === 409 && navigator?.onLine) {
+                    config.evictGETVersion(request.url);
+                } else if (!response.ok || !config.psudo.qualifiedRequestResponsesCode.includes(response.status)) {
+                    config.buildIndexDBRecord(request);
+                } else if (response.ok) {
+                    config.evictGETVersion(request.url);
+                }
 
                 return response;
             } catch (error) {
-                console.log(error);
                 config.buildIndexDBRecord(request);
                 
                 return new Response(null, {status: 422, statusText: config.messages.errors.postRequest});
@@ -68,15 +81,40 @@ const config = {
         }
     },
     evictGETVersion: async function(url) {
+        if (!navigator.onLine) return;
+        
         const cache = await caches.open(config.caches.GETCache);
         const cacheKeys = await cache.keys();
 
-        cacheKeys.map(async item => {
-            if (item.url !== url) return;
+        for (const item of cacheKeys)
+            if (item.url === url) await config.rerunGETRequest(item.url);
 
-            const cachedResponse = await cache.match(item);
-            await cache.delete(cachedResponse.url);
-        });
+        /**
+         * Keep refreshing trip so that the overall
+         * facade remains updated
+         */
+        await config.rerunGETRequest('/trip');
+    },
+    rerunGETRequest: async function(url) {
+        if (!navigator.onLine) return;
+
+        try {
+            const cache = await caches.open(config.caches.GETCache);
+
+            console.log(`Deleting ${url}`);
+
+            await cache.delete(url);
+
+            const {quota, usage} = await navigator.storage.estimate();
+            const freeSpace = quota - usage > 0;
+            
+            const networkResponse = await fetch(url);
+            if (networkResponse.ok && !networkResponse.redirected && freeSpace) await cache.put(url, networkResponse.clone());
+
+            return networkResponse;
+        } catch (error) {
+            
+        }
     },
     buildIndexDBRecord: async function(request) {
         const idb = new IndexedDBManager();
@@ -98,7 +136,7 @@ const config = {
     psudo: {
         login: '/auth/login',
         origin: 'host',
-        qualifiedRequestResponsesCode: [200, 400, 401, 403, 404, 409, 422]
+        qualifiedRequestResponsesCode: [200, 400, 401, 403, 404]
     }
 };
 
